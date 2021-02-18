3001c0a52d59cebb6e13384594269487504b5ba8
gauravkumar02 <gaurav.kumar02@nagarro.com>
2021-02-18 11:33:23 +0530

    first push

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,jsonify,request,render_template,copy_current_request_context
+from flask_cors import CORS,cross_origin
+import json
+import time
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+CORS(app)
+
+@app.route('/gettime' , methods=['GET'])
+@cross_origin()
+def return_time():
+    value = str(time.time())
+    return jsonify({"time": value})
+
+@app.route('/test' , methods=['GET'])
+@cross_origin()
+def test():
+    value = str(time.time())
+    return jsonify({"value": "kubernetes is awesome!!"})
+
+@app.route('/test1' , methods=['GET'])
+@cross_origin()
+def test1():
+    value = str(time.time())
+    return jsonify({"value": "Google gke!!"})
+
+app.run(port=5001,host='0.0.0.0',debug=True)
