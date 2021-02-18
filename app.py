533c66fccbbddd44c5aa7fefea075d60c254fdda
gauravkumar02 <gaurav.kumar02@nagarro.com>
2021-02-18 13:44:21 +0530

    testing with new cloudbuild yaml

--- app.py
+++ app.py
@@ -22,6 +22,6 @@ def test():
 @cross_origin()
 def test1():
     value = str(time.time())
-    return jsonify({"value": "Hi ..Google gke!!"})
+    return jsonify({"value": "Hi ... Google gke!!"})
 
 app.run(port=5001,host='0.0.0.0',debug=True)
